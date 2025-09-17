ef26d36528fc0a7fa78f919925ebd38404964253
arifeen314 <arifeenpie@gmail.com>
2025-09-18 01:41:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -9,15 +8,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-=======
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> e1d13e65624dea6ed8ab69f738a9d619a169671f
