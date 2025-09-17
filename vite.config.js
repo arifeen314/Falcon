2cdc77065a28c97db103251f71c4024f629474fa
arifeen314 <arifeenpie@gmail.com>
2025-09-18 01:22:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: './',
-=======
-  base: './',  // ensures Vite works with nested routes on Vercel
->>>>>>> e1d13e65624dea6ed8ab69f738a9d619a169671f
+  base: './', // ensures Vite works with nested routes on Vercel
 })
